fde7a24d103229ab5c10bd51c08f107aa702b536
Kire <kjire_mitrov@outlook.com>
2019-07-04 22:16:57 +0200

    High and Low temperature added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,9 +12,10 @@ const url = 'https://api.darksky.net/forecast/852d29105323390e5379023b1f9204dd/'
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, {
-                summary: body.daily.data[0].summary,
+                summary: body.daily.data[0].summary + " The high today is: " + body.daily.data[0].temperatureHigh + ", and the low is: " + body.daily.data[0].temperatureLow,
                 temperature: body.currently.temperature,
                 precipProbability: body.currently.precipProbability
+
             })
         }
     
